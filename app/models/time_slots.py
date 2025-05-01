dc81d5556579b9ca9674ebe6a36359f88dadb7c7
Oleksandr Ivaniuk <oleksandr.ivaniuk@roboneers.net>
2025-05-02 01:05:58 +0300

    fixed dynamo

--- app/models/time_slots.py
+++ app/models/time_slots.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
+class TimeSlot(BaseModel):
+    start_time: datetime = Field(..., example="2025-04-20T13:00:00Z")
+    end_time: datetime = Field(..., example="2025-04-20T14:00:00Z")
 
 class TimeSlotIn(BaseModel): # store according to ISO 8601
     start_time: str = Field(..., example="2025-04-20T13:00:00Z")
--- app/slots/main.py
+++ app/slots/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Header, HTTPException, Depends
+import datetime
+from fastapi import FastAPI, Header, HTTPException, Depends, Query
 from app.models.time_slots import TimeSlotIn, TimeSlotOut
 import uuid, os, asyncio
 import aioboto3
@@ -8,36 +9,37 @@ load_dotenv(find_dotenv())
 
 AWS_REGION = os.getenv("AWS_REGION", "eu-west-1")
 TABLE_NAME = os.getenv("SLOTS_TABLE", "TimeSlots")
+DYNAMODB_ENDPOINT = os.getenv("DYNAMODB_ENDPOINT", "http://dynamodb-local:8000")
 
 app = FastAPI(title="TimeSlot Service")
 
 
-def get_current_user(auth_token: str = Header(...)) -> str:
+def get_current_user_email(auth_token: str = Header(...)) -> str:
     from utils import generate_jwt
     try:
         payload = generate_jwt.decode_access_token(auth_token)
     except Exception:
         raise HTTPException(401, "Invalid token")
-    user_id = payload.get("sub")
-    if not user_id:
+    user_email = payload.get("email")
+    if not user_email:
         raise HTTPException(401, "Token has no subject")
-    return user_id
+    return user_email
 
 async def get_dynamo():
     session = aioboto3.Session()
-    async with session.resource("dynamodb", region_name=AWS_REGION, endpoint_url=os.getenv("DYNAMODB_ENDPOINT")) as dynamo:
-      table = await dynamo.Table(TABLE_NAME)
+    async with session.resource("dynamodb", region_name=AWS_REGION, endpoint_url=DYNAMODB_ENDPOINT) as dynamo:
+      table = await dynamo.Table("TimeSlots")
       yield table
 
 @app.post("/slots", response_model=TimeSlotOut)
 async def create_slot(
     slot: TimeSlotIn,
-    user_id: str = Depends(get_current_user),
+    user_email: str = Depends(get_current_user_email),
     table = Depends(get_dynamo),
 ):
     slot_id = str(uuid.uuid4())
     item = {
-        "userId":    user_id,
+        "userEmail":    user_email,
         "slotId":    slot_id,
         "startTime": slot.start_time,
         "endTime":   slot.end_time,
@@ -47,12 +49,12 @@ async def create_slot(
 
 @app.get("/slots", response_model=list[TimeSlotOut])
 async def list_slots(
-    user_id: str = Depends(get_current_user),
+    user_email: str = Depends(get_current_user_email),
     table = Depends(get_dynamo),
 ):
     resp = await table.query(
-        KeyConditionExpression="userId = :u",
-        ExpressionAttributeValues={":u": user_id}
+        KeyConditionExpression="userEmail = :u",
+        ExpressionAttributeValues={":u": user_email}
     )
     return [
         TimeSlotOut(
@@ -61,12 +63,50 @@ async def list_slots(
             end_time=i["endTime"],
         ) for i in resp.get("Items", [])
     ]
+    
+@app.get("/slots/free", response_model=list[TimeSlotOut])
+async def get_free_slots(
+    time_interval: TimeSlotIn,
+    user_email: str = Depends(get_current_user_email),
+    table = Depends(get_dynamo),
+):
+    start_time = time_interval.start_time
+    end_time = time_interval.end_time
+    
+    resp = await table.query(
+        KeyConditionExpression="userEmail = :u",
+        ExpressionAttributeValues={":u": user_email}
+    )
+    
+    booked_slots = [
+        TimeSlotIn(
+            start_time=i["startTime"],
+            end_time=i["endTime"],
+        ) for i in resp.get("Items", [])
+    ]
+
+    print("booked_slots", booked_slots)
+    
+    booked_slots = sorted(booked_slots, key=lambda x: x.start_time)
+    booked_slots = list(filter(lambda x: x.end_time < end_time and x.start_time > start_time, booked_slots))
+    free_slots: list[TimeSlotOut] = []
+    for i in range(len(booked_slots) - 2):
+        free_slots.append(
+            TimeSlotOut(
+                slot_id=str(uuid.uuid4()),
+                start_time = booked_slots[i].end_time,
+                end_time = booked_slots[i + 1].start_time,
+            )
+        )
+    
+    return free_slots
+    
 
 @app.delete("/slots/{slot_id}")
 async def delete_slot(
     slot_id: str,
-    user_id: str = Depends(get_current_user),
+    user_email: str = Depends(get_current_user_email),
     table = Depends(get_dynamo),
 ):
-    await table.delete_item(Key={"userId": user_id, "slotId": slot_id})
+    await table.delete_item(Key={"userEmail": user_email, "slotId": slot_id})
     return {"deleted": slot_id}
